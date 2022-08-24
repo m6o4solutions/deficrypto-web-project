e90addc90e4a65e72b693823565ac49670db46a6
Michael Owuor <michael.owuor@gmail.com>
2022-08-24 16:45:54 +0300

    feat: create signup section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/navbar';
 import Hero from './components/hero';
 import Feature from './components/feature';
+import Signup from './components/signup';
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Navbar />
 			<Hero />
 			<Feature />
+			<Signup />
 		</>
 	);
 }
