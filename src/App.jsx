3e327de00cbdfd60162e2f22cc0532940e169308
Michael Owuor <michael.owuor@gmail.com>
2022-08-24 16:04:51 +0300

    feat: create feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/navbar';
 import Hero from './components/hero';
+import Feature from './components/feature';
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Hero />
+			<Feature />
 		</>
 	);
 }
